2eedb11f66772172959071cdba641b404f502b79
Jhadeepu <126418362+Jhadeepu@users.noreply.github.com>
2023-06-06 02:55:16 +0530

    Update session.js

--- sportsschedule/models/session.js
+++ sportsschedule/models/session.js
@@ -1,63 +1,26 @@
-// Session.js
-
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../path/to/db');
-const Sport = require('./Sport');
-const User = require('./User');
-
-const Session = sequelize.define('Session', {
-  // Define the model attributes
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  sessionDateTime: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  sessionVenue: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  sessionParticipants: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  playersNeeded: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  createdBy: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: User,
-      key: 'id',
-    },
-  },
-  sportId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Sport,
-      key: 'id',
-    },
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-  },
-});
-
-// Establish associations
-Session.belongsTo(User, { foreignKey: 'createdBy' });
-Session.belongsTo(Sport, { foreignKey: 'sportId' });
-
-module.exports = Session;
+/* eslint-disable no-unused-vars */
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Session extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Session.init({
+    name: DataTypes.STRING,
+    createdBy: DataTypes.STRING,
+    sportId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Session',
+  });
+  return Session;
+};
