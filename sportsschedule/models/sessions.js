cd28eff9fb1e3e14f9795c03101e8e30e545e855
Jhadeepu <126418362+Jhadeepu@users.noreply.github.com>
2023-06-06 16:19:33 +0530

    Update sessions.js

--- sportsschedule/models/sessions.js
+++ sportsschedule/models/sessions.js
@@ -11,11 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      sessions.belongsTo(models.Sport, {
-        foreignKey: "sportId",
-      });
-    }
   }
   sessions.init({
     date: DataTypes.DATE,
