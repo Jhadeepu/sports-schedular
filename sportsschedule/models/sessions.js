2b1b9e5bd3b8dcb4675a92b091f5905400bbf944
Jhadeepu <126418362+Jhadeepu@users.noreply.github.com>
2023-06-06 16:24:33 +0530

    Update sessions.js

--- sportsschedule/models/sessions.js
+++ sportsschedule/models/sessions.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static associate(models) {
+    }
   }
   sessions.init({
     date: DataTypes.DATE,
